b05507a88ea06e2057159c6e39e7f18d4013319b
NotJL <58279268+notjl@users.noreply.github.com>
2022-09-29 17:00:51 +0800

    Fixes VT closing file-like objects

--- backend/routers/library.py
+++ backend/routers/library.py
@@ -4,8 +4,11 @@ from fastapi import APIRouter, Depends, UploadFile, File
 from motor.motor_asyncio import AsyncIOMotorCollection
 
 
+from ..utils import checks
 from ..database import schemas, db
 from ..handlers import lib_handlers as handler
+
+professor_access = checks.RoleChecker(["professor"])
 
 router = APIRouter(
     prefix="/library",
@@ -22,6 +25,7 @@ async def upload(
     book: schemas.Book = Depends(schemas.Book.as_form),
     ebook: UploadFile = File(...),
     collection: AsyncIOMotorCollection = Depends(db.get_ebooks_collection),
+    access = Depends(professor_access)
 ) -> schemas.ShowBook:
     return await handler.upload_ebook(
         book, ebook, collection, db.get_ebooks_gridfs()
